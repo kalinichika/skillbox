6954ebada7b355148421b9889fa483112889b701
Vika <viktoriya.v.kalinina@yandex.ru>
2020-10-07 08:44:34 +0300

    Les -5, mod - 2. First addition css&style loaders.

--- src/shared/Header.jsx
+++ src/shared/Header.jsx
@@ -1,18 +1,20 @@
-import { hot } from 'react-hot-loader/root';
-import * as React from 'react';
-
+import { hot } from "react-hot-loader/root";
+import * as React from "react";
+import "./Header.css";
 function HeaderComponent() {
-	// const [state, setState] = React.useState(false);
-	return (
-		<header>
-			<h2>Skillbox lessons started!</h2>
-			{/* <button onClick={() => setState(!state)}> */}
-			{/* {state ? "hide" : "show"} */}
-			{/* </button> */}
-			{/* {state && <h3> Here i'm</h3>} */}
-			<p>It replace code without page reloading</p>
-		</header>
-	);
+  // const [state, setState] = React.useState(false);
+  return (
+    <header>
+      <h2 className={'text'}>Skillbox lessons started!</h2>
+      {/* <button onClick={() => setState(!state)}> */}
+      {/* {state ? "hide" : "show"} */}
+      {/* </button> */}
+      {/* {state && <h3> Here i'm</h3>} */}
+      <p className={'text'}>
+        It replace code without page reloading
+      </p>
+    </header>
+  );
 }
 
 // export default HeaderComponent;
