40b12fbaf02d1961e81c8769967bfa3dd187f317
kalinichika <viktoriya.v.kalinina@yandex.ru>
2020-11-14 14:35:09 +0300

    Add Text componenet with classnames module

---
+++ src/shared/Text/Text.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styles from './text.css';
+import classNames from 'classnames';
+
+export enum EColor {
+  black = 'black',
+  orange = 'orange',
+  green = 'green',
+  white = 'white',
+  greyF4 = 'greyF4',
+  greyF3 = 'greyF3',
+  greyD9 = 'greyD9',
+  greyC4 = 'greyC4',
+  grey99 = 'grey99',
+  grey66 = 'grey66',
+}
+
+type TSizes = 28 | 20 | 16 | 14 | 12 | 10;
+
+interface ITextProps {
+  As?: 'span' | 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'div';
+  children?: React.ReactNode;
+  bold?: boolean;
+  size: TSizes;
+  mobileSize?: TSizes;
+  tabletSize?: TSizes;
+  desktopSize?: TSizes;
+  color?: EColor;
+}
+
+export function Text(props: ITextProps) {
+  const {
+    As = 'span',
+    color = EColor.black,
+    bold = false,
+    children,
+    size,
+    mobileSize,
+    tabletSize,
+    desktopSize,
+  } = props;
+
+  const classes = classNames(
+    styles[color],
+    { [styles.bold]: bold },
+    styles[`s${size}`],
+    { [styles[`m${mobileSize}`]]: mobileSize },
+    { [styles[`t${tabletSize}`]]: tabletSize },
+    { [styles[`d${desktopSize}`]]: desktopSize }
+  );
+
+  console.log(classes);
+
+  return <As className={classes}>{children}</As>;
+}
